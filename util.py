267f05445f03bd6bd87d263b013ba01d7bc3903b
Siddharth Nand <snand233@gmail.com>
2024-01-05 22:08:41 -0800

    changed last update file to json

--- util.py
+++ util.py
@@ -1,13 +1,22 @@
 import datetime
 import os
+import json
 
-LAST_UPDATE_PATH = os.path.join("./data/last_update.txt")
+LAST_UPDATE_PATH = os.path.join("./data/last_update.json")
 EXPORT_PATH = os.path.join("./data")
 
-def update():
-    last_update = read_data(LAST_UPDATE_PATH)
+def update(type):
+    last_update = json.loads(read_data(LAST_UPDATE_PATH))
+    today = datetime.datetime.now().date()
+    today_str = str(today)
 
-    if len(last_update) == 0 or str(last_update) != str(datetime.datetime.now().date()):
+    if type not in last_update or last_update[type] != today_str:
+        last_update[type] = today_str
+    
+        f = open(LAST_UPDATE_PATH, "w")
+        f.write(json.dumps(last_update))
+        f.close()
+    
         return True
     
     return False
