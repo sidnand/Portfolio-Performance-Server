2af785f770022bf69eb41ec8aca866917d66e893
Siddharth Nand <snand233@gmail.com>
2024-01-06 15:50:01 -0800

    added 25 portfolio, factors and industry support

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import http.server
 import socketserver
 import json
 import os
+from ken_r_french import *
 
 from sp_sector import get_sp_sector
 
@@ -9,17 +10,38 @@ PORT = 8000
 
 class APIServerHandler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
-        if self.path == '/spsector':
-            self.send_response(200)
+        self.send_response(200)
             
-            self.send_header('Content-type', 'application/json')
-            self.send_header('Access-Control-Allow-Origin', '*')
-            self.send_header('Access-Control-Allow-Methods', 'GET')
-            self.send_header('Access-Control-Allow-Headers', 'Content-Type')
-            self.end_headers()
-            
+        self.send_header('Content-type', 'application/json')
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header('Access-Control-Allow-Methods', 'GET')
+        self.send_header('Access-Control-Allow-Headers', 'Content-Type')
+        self.end_headers()
+        
+        if self.path == '/spsector':            
             data = get_sp_sector()
             self.wfile.write(json.dumps(data).encode())
+
+        elif self.path == '/25_1':
+            data = get_25_1()
+            self.wfile.write(json.dumps(data).encode())
+
+        elif self.path == '/25_3':
+            data = get_25_3()
+            self.wfile.write(json.dumps(data).encode())
+
+        elif self.path == '/25_4':
+            pass
+        elif self.path == '/ff4':
+            data = get_3_factors()
+            self.wfile.write(json.dumps(data).encode())
+
+        elif self.path == '/industry':
+            data = get_10_industry_riskfree()
+            self.wfile.write(json.dumps(data).encode())
+
+        elif self.path == '/international':
+            pass
         else:
             self.send_response(404)
             self.end_headers()
