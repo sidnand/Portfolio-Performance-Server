60428332b7c3e6b7e878f9ced55b4bbb794b2abd
Siddharth Nand <snand233@gmail.com>
2024-01-05 22:37:57 -0800

    fixed download bugs

--- ken_r_french.py
+++ ken_r_french.py
@@ -1,18 +1,17 @@
-import datetime
 import os
 import pandas as pd
-import numpy as np
 import zipfile
 import requests
-from io import BytesIO
+from io import BytesIO, StringIO
+
+from util import *
 
 URL_25_PORTFOLIO =  "https://mba.tuck.dartmouth.edu/pages/faculty/ken.french/ftp/25_Portfolios_5x5_CSV.zip"
 URL_10_INDUSTRY = "https://mba.tuck.dartmouth.edu/pages/faculty/ken.french/ftp/10_Industry_Portfolios_CSV.zip"
 URL_3_FACTORS = "https://mba.tuck.dartmouth.edu/pages/faculty/ken.french/ftp/Developed_3_Factors_CSV.zip"
 
 def download(url):
-    # comment
-    response = requests.get(URL_25_PORTFOLIO)
+    response = requests.get(url)
     zip_file = zipfile.ZipFile(BytesIO(response.content))
     zip_file.extractall()
 
@@ -24,16 +23,26 @@ def download(url):
         return file_content
 
 def get_25_portfolio():
-    data = download(URL_25_PORTFOLIO)
-    print(data)
+    return get("25_portfolio", URL_25_PORTFOLIO)
 
 
 def get_10_industry():
-    data = download(URL_10_INDUSTRY)
-    print(data)
+    return get("10_industry", URL_10_INDUSTRY)
 
 def get_3_factors():
-    data = download(URL_3_FACTORS)
-    print(data)
+    return get("3_factors", URL_3_FACTORS)
 
-# get_25_portfolio()
+def get(type, url):
+    if update(type):
+        print(f"Downloading {type} data...")
+        data = download(url)
+        data_start = data.find("Average Value Weighted Returns -- Monthly") if type != "3_factors" else data.find("Missing data are indicated by -99.99.")
+        data_end = data.find("Average Equal Weighted Returns -- Monthly") if type != "3_factors" else data.find("Annual Factors: January-December")
+        data = data[data_start:data_end]
+        df = pd.read_csv(StringIO(data), skiprows=1, engine='python', index_col=0)
+        df.to_csv(os.path.join(EXPORT_PATH, f"{type}.csv"))
+
+    print("Data up to date.")
+
+    data = pd.read_csv(os.path.join(EXPORT_PATH, f"{type}.csv"), index_col=0)
+    return data
